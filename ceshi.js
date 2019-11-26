f36230a165a4a5c2cf42097b2be40931d60ff0fe
cl0925 <cl0925@live.cn>
2019-11-26 21:00:27 +0800

    add

---
+++ ceshi.js
@@ -0,0 +1,121 @@
+var obj = JSON.parse($response.body);
+obj = {
+    "code": 0,
+ "data": [
+  {
+   "name": " ",
+   "pic": " ",
+   "url": " ",
+   "ad": 0
+  },
+  {
+   "name": " ",
+   "pic": " ",
+   "url": " ",
+   "ad": 0
+  },
+  {
+   "name": " ",
+   "pic": " ",
+   "url": " ",
+   "ad": 0
+  },
+  {
+   "id": "15460",
+   "cid": "24",
+   "name": "辅佐官2：改变世界的人们",
+   "pic": "https://tu.511.la/uploads/191112/121153535989.jpg",
+   "pic2": "https://tu.511.la/uploads/191112/120337121135.jpg",
+   "info": "高清",
+   "state": "更新至5集/共16集",
+   "type": "韩剧",
+   "hits": "1.88万",
+   "pf": "8.9",
+   "vip": "0",
+   "cion": "0",
+   "fid": "1",
+   "ad": 0
+  },
+  {
+   "id": "15315",
+   "cid": "5",
+   "name": "黑暗物质三部曲第一季",
+   "pic": "https://pic.yefu365.com/upload/vod/20191104-1/74f06fd9dc0875f2a5c2e9be947bba79.jpg",
+   "pic2": "https://tu.511.la/uploads/191112/120441289907.jpg",
+   "info": "高清",
+   "state": "更新至4集/共8集",
+   "type": "科幻动作",
+   "hits": "4.69万",
+   "pf": "7.6",
+   "vip": "0",
+   "cion": "0",
+   "fid": "2",
+   "ad": 0
+  },
+  {
+   "id": "15448",
+   "cid": "11",
+   "name": "无耻之徒美版第十季",
+   "pic": "https://pic.rrzy.cc/upload/vod/20191110-1/34f40b7880cf6c2db7334a782a05f2eb.jpg",
+   "pic2": "https://tu.511.la/uploads/191112/120451201211.jpg",
+   "info": "高清",
+   "state": "更新至3集",
+   "type": "喜剧青春",
+   "hits": "5.82万",
+   "pf": "9.7",
+   "vip": "0",
+   "cion": "0",
+   "fid": "2",
+   "ad": 0
+  },
+  {
+   "id": "16516",
+   "cid": "15",
+   "name": "第一滴血5：最后的血",
+   "pic": "http://pic.yefu365.com/upload/vod/20191123-1/ec7eb8ea95614d92596683ee2c0c8e0d.jpg",
+   "pic2": "https://tu.511.la/uploads/191123/230844453190.jpg",
+   "info": "高清",
+   "state": "完结",
+   "type": "动作片",
+   "hits": "3.44万",
+   "pf": "7.6",
+   "vip": "0",
+   "cion": "0",
+   "fid": "1",
+   "ad": 0
+  },
+  {
+   "id": "16495",
+   "cid": "15",
+   "name": "双子杀手",
+   "pic": "http://pic.yefu365.com/upload/vod/20191122-1/e929d08267203d7922168e297e8fe9af.jpg",
+   "pic2": "https://tu.511.la/uploads/191123/230152144757.jpg",
+   "info": "高清",
+   "state": "完结",
+   "type": "动作片",
+   "hits": "4.13万",
+   "pf": "7.1",
+   "vip": "0",
+   "cion": "0",
+   "fid": "1",
+   "ad": 0
+  },
+  {
+   "id": "15442",
+   "cid": "20",
+   "name": "小丑2019（韩版中字）",
+   "pic": "https://pic.rrzy.cc/upload/vod/20191110-1/1d8f647b44f9d157ba518af07c4fef80.jpg",
+   "pic2": "https://tu.511.la/uploads/191110/100335443658.jpg",
+   "info": "韩版中字",
+   "state": "完结",
+   "type": "恐怖片",
+   "hits": "26.15万",
+   "pf": "9.2",
+   "vip": "0",
+   "cion": "0",
+   "fid": "1",
+   "ad": 0
+  }
+ ]
+}
+$done({body: JSON.stringify(obj)});
