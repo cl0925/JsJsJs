e939acdb24c20851a0efe474c140122bc32c75f8
cl0925 <cl0925@live.cn>
2020-01-14 17:05:20 +0800

    add

--- jrrb.js
+++ jrrb.js
@@ -1,18 +1,10 @@
-/*
-今日热榜
-https:\/\/api\.tophub\.today\/account\/sync url script-response-body jrrb.js
-hostname= api.tophub.today
-@supported 96440338D30B
-*/
-var obj = JSON.parse($response.body);
- obj={
- "data": {
-  "is_vip": "1",
-  "vip_expired": "4092599349",
-  "is_vip_now": true
- },
- "error": false,
- "status": 200
-};
+//今日热榜 crate by makexp 2020.01.14
+//^https:\/\/api\.tophub\.today\/account\/sync* url script-response-body jrrb.js
+
+let obj = JSON.parse($response.body);
+obj["data"]={
+	"vip_expired": "18630733746",
+	"is_vip_now": true,
+	"is_vip": "1"
+	}
 $done({body: JSON.stringify(obj)});
-//
